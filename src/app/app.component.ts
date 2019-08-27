92e565cf044c95bf572c4f05c66431c4e1737412
Melisa C <salas@unilibre.edu.co>
2019-08-26 20:39:34 -0500

    Primera version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pelele';
+
+  
+  lat = 51.678418;
+  lng = 7.809007;
+
+
+  nombre: string= ''
+  paises: any =[
+
+    {activo: true,nombre:'Colombia'},
+    {activo: false,nombre:'Argentina'},
+    {activo: true,nombre:'Brasil'},
+    {activo: false,nombre:'Peru'}
+
+
+   
+
+   ]
+
 }
